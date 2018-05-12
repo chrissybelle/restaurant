d5741c878b8e144631074650cbb26abc822c42af
cbelle <christabelle.chien@gmail.com>
2018-05-12 12:27:46 -0400

    updates

---
+++ restaurant.js
@@ -0,0 +1,18 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+var path = require("path");
+
+var app = express();
+var PORT = process.env.PORT || 3000;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.get("/", function(req, res) {
+    res.sendFile
+});
+
+app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+  });
+  
